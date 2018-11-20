45e6574b5957c42d3418eb052b980c834f7748bb
saanikagupta <33591235+saanikagupta@users.noreply.github.com>
2018-11-20 23:36:45 +0530

    Create Application2.c

---
+++ Binary Search/Application2.c
@@ -0,0 +1,132 @@
+/*Given an array of n elements. Find whether there are two elements in the array such that
+their sum is equal to given element K or not? in O(nlogn) time.
+*/
+#include<stdio.h>
+int n;
+int merge(int a[],int low,int mid,int high){
+	int i=low,j=mid+1,c[n],k=low;
+	while((i<=mid) && (j<=high)){
+		if(a[i]<=a[j]){
+			c[k]=a[i];
+			i=i+1;
+			k=k+1;
+		}
+
+		else{
+			c[k]=a[j];
+			k=k+1;
+			j=j+1;
+		}
+	}
+
+	while(i<=mid){
+		c[k]=a[i];
+		i=i+1;
+		k=k+1;
+	}
+
+	while(j<=high){
+		c[k]=a[j];
+		j=j+1;
+		k=k+1;
+	}
+
+	for(i=low;i<=high;i++){
+		a[i]=c[i];
+	}
+	return 0;
+}
+
+
+int mergeSort(int a[],int low,int high){
+	int mid=0;
+	if(low<high){
+		mid=(low+high)/2;
+
+		mergeSort(a,low,mid);
+		mergeSort(a,mid+1,high);
+
+		merge(a,low,mid,high);
+	}
+	return 0;
+}
+
+int main(){
+    int i;
+    printf("\nEnter the size of array:\n");
+    scanf("%d",&n);
+    int a[n];
+    printf("\nEnter the elements:\n");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    mergeSort(a,0,n-1);
+    int s,flag=0,mid,high,low;
+    printf("\nEnter the sum\n");
+    scanf("%d",&s);
+    int newkey;
+    if(n>=2){
+        int max=a[n-1]+a[n-2];
+        int min=a[0]+a[1];
+    
+        if(s>max || s<min){
+    	    printf("Not Found\n");
+    	    flag=1;
+        }
+    }
+	if(flag==0){
+		for(i=0;i<n;i++){
+			high=n-1;
+			low=0;
+			newkey=s-a[i];
+			while(low<=high){
+				mid=(high+low)/2;
+
+				if(a[mid]==newkey && i!=mid){
+					printf("\nFound\n");
+					flag=1;
+					break;
+				}
+
+				else if(a[mid]>newkey)
+					high=mid-1;
+
+				else
+					low=mid+1;
+			}
+			if(flag==1)
+			    break;	
+        }
+	}        	
+	
+	if(flag==0)
+	    printf("\nNot Found\n");
+	return 0;
+}
+
+/*Implementation-
+
+Example-1
+Enter the size of array:
+8
+
+Enter the elements:
+2 -1 0 2 1 33 1 8
+
+Enter the sum
+7
+
+Found
+
+Example-2
+Enter the size of array:
+8  
+
+Enter the elements:
+2 -1 0 2  1 33 1 8
+
+Enter the sum
+31
+
+Not Found
+
+*/
